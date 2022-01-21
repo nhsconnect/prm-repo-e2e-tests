9d418b1b2c8640142b4ada5e62df1a16e586193b
Rosie Amphlett <rosie.amphlett1@nhs.net>
2022-01-21 12:24:49 +0000

    [PRMT-2305] Added test for non-synthetic patient event when synthetic toggle is on and updated test data for dev env

---
+++ src/test/java/uk/nhs/prm/deduction/e2e/suspensions/MofNotUpdatedMessageQueue.java
@@ -0,0 +1,30 @@
+package uk.nhs.prm.deduction.e2e.suspensions;
+
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sqs.model.Message;
+import uk.nhs.prm.deduction.e2e.TestConfiguration;
+import uk.nhs.prm.deduction.e2e.queue.SqsQueue;
+
+import java.util.List;
+
+@Component
+public class MofNotUpdatedMessageQueue extends SuspensionMessageQueue{
+
+    @Autowired
+    public MofNotUpdatedMessageQueue(SqsQueue sqsQueue, TestConfiguration configuration) {
+        super(sqsQueue, configuration.mofNotUpdatedQueueUri());
+    }
+
+    public boolean containsMessage(List<Message> messages, String nhsNumber) throws JSONException {
+        for (Message message: messages) {
+            if(MofUpdateMessage.parseMessage(message.body()).nhsNumber().contains(nhsNumber)) {
+                log("Message present on queue");
+                sqsQueue.deleteMessage(queueUri,message);
+                return true;
+            }
+        }
+        return false;
+    }
+}
