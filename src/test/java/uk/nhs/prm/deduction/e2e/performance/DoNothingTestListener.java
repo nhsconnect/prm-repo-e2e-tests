53a3a90d0222dd471125fc90773ff6a2a3c70bb9
Dan Moore <dan.moore1@nhs.net>
2022-02-01 13:55:46 +0000

    [PRMT-2484] Trace test messages in NemsTestEvent objects; output duration

--- src/test/java/uk/nhs/prm/deduction/e2e/performance/DoNothingTestListener.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/performance/DoNothingTestListener.java
@@ -2,12 +2,12 @@ package uk.nhs.prm.deduction.e2e.performance;
 
 public class DoNothingTestListener implements NemsPatientEventTestListener {
     @Override
-    public void onStartingTestItem(String nemsMessageId, String nhsNumber) {
+    public void onStartingTestItem(NemsTestEvent testEvent) {
         // nop
     }
 
     @Override
-    public void onStartedTestItem(String nemsMessageId, String nhsNumber) {
+    public void onStartedTestItem(NemsTestEvent testEvent) {
         // nop
     }
 }
---
+++ src/test/java/uk/nhs/prm/deduction/e2e/performance/NemsTestEvent.java
@@ -0,0 +1,49 @@
+package uk.nhs.prm.deduction.e2e.performance;
+
+import uk.nhs.prm.deduction.e2e.queue.SqsMessage;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import static java.time.LocalTime.now;
+
+public class NemsTestEvent {
+    private final String nemsMessageId;
+    private final String nhsNumber;
+
+    private final LocalTime createdAt;
+    private LocalDateTime started;
+
+    public NemsTestEvent(String nemsMessageId, String nhsNumber) {
+        this.nemsMessageId = nemsMessageId;
+        this.nhsNumber = nhsNumber;
+        this.createdAt = now();
+    }
+
+    public String nemsMessageId() {
+        return nemsMessageId;
+    }
+
+    public String nhsNumber() {
+        return nhsNumber;
+    }
+
+    public LocalDateTime startedAt() {
+        return started;
+    }
+
+    public void started() {
+        this.started = LocalDateTime.now();
+    }
+
+    public void finished(SqsMessage successMessage) {
+        var processingTimeMs = startedAt().until(successMessage.queuedAt(), ChronoUnit.MILLIS);
+
+        System.out.println(String.format("NEMS suspension %s was injected at %tT and arrived on output queue at %tT after %s ms",
+                nemsMessageId(),
+                startedAt(),
+                successMessage.queuedAt(),
+                processingTimeMs));
+    }
+}
