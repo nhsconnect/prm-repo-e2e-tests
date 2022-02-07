d3fb57a733ab5a9917613faf615e330ba293c224
Dan Moore <dan.moore1@nhs.net>
2022-02-07 11:25:12 +0000

    [PRMT-2484] LoadPhase doing more; perf test multiple flat rates

--- src/test/java/uk/nhs/prm/deduction/e2e/performance/LoadRegulatingPool.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/performance/LoadRegulatingPool.java
@@ -4,19 +4,16 @@ import uk.nhs.prm.deduction.e2e.timing.Timer;
 import uk.nhs.prm.deduction.e2e.timng.Sleeper;
 
 import java.io.PrintStream;
-import java.math.BigDecimal;
 import java.util.List;
 
 public class LoadRegulatingPool<T> implements FinitePool<T>, Reportable {
-    private static final BigDecimal ONE_THOUSAND = new BigDecimal(1000);
-
     private int count;
     private final Pool<T> sourcePool;
     private List<LoadPhase> phases;
     private int phaseIndex;
-    private Long lastItemTimeMillis = null;
     private Timer timer;
     private Sleeper sleeper;
+    private Long lastItemTimeMillis = null;
 
     public LoadRegulatingPool(Pool<T> sourcePool, List<LoadPhase> phases) {
         this(sourcePool, phases, new Timer(), new Sleeper());
@@ -34,36 +31,15 @@ public class LoadRegulatingPool<T> implements FinitePool<T>, Reportable {
     @Override
     public T next() {
         long nowMilliseconds = timer.milliseconds();
-        applyDelay(nowMilliseconds);
+        lastItemTimeMillis = currentPhase().applyDelay(nowMilliseconds, sleeper, lastItemTimeMillis);
         count++;
         currentPhase().incrementPhaseCount();
         return sourcePool.next();
     }
 
-    private void applyDelay(long startTime) {
-        if (lastItemTimeMillis == null) {
-            lastItemTimeMillis = startTime;
-            return;
-        }
-        var elapsed = startTime - lastItemTimeMillis;
-        int requiredDelay = (int) (targetDelayMilliseconds() - elapsed);
-        if (requiredDelay > 0) {
-            sleeper.sleep(requiredDelay);
-        }
-        lastItemTimeMillis = startTime + requiredDelay;
-    }
-
-    private int targetDelayMilliseconds() {
-        return ONE_THOUSAND.multiply(BigDecimal.ONE.divide(currentPhase().ratePerSecond)).intValue();
-    }
-
     @Override
     public boolean unfinished() {
         return currentPhase() != null;
-    }
-
-    private boolean phaseFinished() {
-        return currentPhase().count >= currentPhase().maxItems;
     }
 
     @Override
@@ -73,8 +49,7 @@ public class LoadRegulatingPool<T> implements FinitePool<T>, Reportable {
 
     private LoadPhase currentPhase() {
         LoadPhase phase = phases.get(phaseIndex);
-        if (phase.count >= phase.maxItems) {
-            // oops next phase
+        if (phase.finished()) {
             if (phases.size() > phaseIndex + 1) {
                 phaseIndex++;
                 return phases.get(phaseIndex);
