3b7bb3e7e3e8be8f8b7379ca1eaa35d96ede03b6
Dan Moore <dan.moore1@nhs.net>
2022-02-09 18:22:06 +0000

    [PRMT-2484] Make the auto-refresh auth SQS client the default

--- src/test/java/uk/nhs/prm/deduction/e2e/queue/SqsClient.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/queue/SqsClient.java
@@ -7,9 +7,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SqsClient {
-    software.amazon.awssdk.services.sqs.SqsClient sqsClient = software.amazon.awssdk.services.sqs.SqsClient.create();
-
     private final AssumeRoleAuthAutoRefreshSqsClientFactory autoRefreshClientFactory = new AssumeRoleAuthAutoRefreshSqsClientFactory();
+    private final software.amazon.awssdk.services.sqs.SqsClient sqsClient = autoRefreshClientFactory.createAssumeRoleAutoRefreshSqsClient();
 
     public List<SqsMessage> readMessagesFrom(String queueUrl) {
         var receiveMessageRequest = ReceiveMessageRequest.builder()
@@ -51,15 +50,7 @@ public class SqsClient {
     }
 
     private ReceiveMessageResponse receiveMessages(ReceiveMessageRequest receiveMessageRequest) {
-        try {
-            return sqsClient.receiveMessage(receiveMessageRequest);
-        }
-        catch (SqsException e) {
-            System.out.println("Updating client to use assume role refresh request");
-            sqsClient = autoRefreshClientFactory.createAssumeRoleAutoRefreshSqsClient();
-            System.out.println("Attempting receive messages with updated assume role credentials provider");
-            return sqsClient.receiveMessage(receiveMessageRequest);
-        }
+        return sqsClient.receiveMessage(receiveMessageRequest);
     }
 
 }
