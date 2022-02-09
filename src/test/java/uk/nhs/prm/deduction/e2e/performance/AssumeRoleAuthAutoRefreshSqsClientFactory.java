c467a51371a82dc494fa752ff400bde90ac4ffca
Dan Moore <dan.moore1@nhs.net>
2022-02-09 18:17:20 +0000

    [PRMT-2484] Hopefully fix SQS client refresh auth, add in STS client

--- src/test/java/uk/nhs/prm/deduction/e2e/performance/AssumeRoleAuthAutoRefreshSqsClientFactory.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/performance/AssumeRoleAuthAutoRefreshSqsClientFactory.java
@@ -1,23 +1,36 @@
 package uk.nhs.prm.deduction.e2e.performance;
 
+import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
 import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
 
 import static software.amazon.awssdk.regions.Region.EU_WEST_2;
 
-public class StsSessionTokenClient {
+public class AssumeRoleAuthAutoRefreshSqsClientFactory {
 
     private final StsClient stsClient;
     private final String currentRoleArn;
 
-    public StsSessionTokenClient() {
+    public AssumeRoleAuthAutoRefreshSqsClientFactory() {
         this.stsClient = StsClient.builder()
                 .region(EU_WEST_2)
                 .build();
         this.currentRoleArn = stsClient.getCallerIdentity().arn();
     }
 
-    public AssumeRoleRequest refreshAssumeRoleRequest(String roleSessionName) {
+    public SqsClient createAssumeRoleAutoRefreshSqsClient() {
+        return SqsClient.builder().credentialsProvider(StsAssumeRoleCredentialsProvider
+                .builder()
+                .stsClient(stsClient)
+                .refreshRequest(() -> {
+                    System.out.println("Creating refresh session token request");
+                    return refreshAssumeRoleRequest("performance-test");
+                })
+                .build()).build();
+    }
+
+    private AssumeRoleRequest refreshAssumeRoleRequest(String roleSessionName) {
         return AssumeRoleRequest.builder()
                 .roleArn(extractRoleArnFromCurrentRoleArn(currentRoleArn))
                 .roleSessionName(roleSessionName)
