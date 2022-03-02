01d86f899a27f5c7c606c018e56d6b8e8e1b5065
syedsadiqali <syed.ali69@nhs.net>
2022-03-02 10:55:16 +0000

    [PRMT-2420] Added timestamp now to make sure update messages are always later in time stamp so they are not out of order

--- src/test/java/uk/nhs/prm/deduction/e2e/utility/NemsEventFactory.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/utility/NemsEventFactory.java
@@ -3,6 +3,8 @@ package uk.nhs.prm.deduction.e2e.utility;
 import uk.nhs.prm.deduction.e2e.nems.NemsEventMessage;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,20 +13,22 @@ import static uk.nhs.prm.deduction.e2e.nhs.NhsIdentityGenerator.randomNhsNumber;
 
 public class NemsEventFactory {
 
-    public static NemsEventMessage createNemsEventFromTemplate(String nemsEventFilename, String nhsNumber, String nemsMessageId) {
-        return createNemsEventFromTemplate(nemsEventFilename, nhsNumber, nemsMessageId, "B85612");
+    public static NemsEventMessage createNemsEventFromTemplate(String nemsEventFilename, String nhsNumber, String nemsMessageId,String timestamp) {
+        return createNemsEventFromTemplate(nemsEventFilename, nhsNumber, nemsMessageId, "B85612",timestamp);
     }
 
-    public static NemsEventMessage createNemsEventFromTemplate(String nemsEventFilename, String nhsNumber, String nemsMessageId, String previousGP) {
+    public static NemsEventMessage createNemsEventFromTemplate(String nemsEventFilename, String nhsNumber, String nemsMessageId, String previousGP, String timestamp) {
         return new NemsEventMessage(Resources.readTestResourceFile(nemsEventFilename)
                 .replaceAll("__NHS_NUMBER__", nhsNumber)
                 .replaceAll("__NEMS_MESSAGE_ID__", nemsMessageId)
                 .replaceAll("__PREVIOUS_GP_ODS_CODE__", previousGP)
+                .replaceAll("__LAST-UPDATED__",timestamp)
         );
     }
 
     public static Map<String, NemsEventMessage> getDLQNemsEventMessages() throws IOException {
-        NemsEventMessage nhsNumberVerification = createNemsEventFromTemplate("nhs-number-verification-fail.xml", randomNhsNumber(), randomNemsMessageId());
+        Timestamp now = Timestamp.from(Instant.now());
+        NemsEventMessage nhsNumberVerification = createNemsEventFromTemplate("nhs-number-verification-fail.xml", randomNhsNumber(), randomNemsMessageId(),now.toString());
 //        NemsEventMessage nhsNumberFieldNotPresent = createNemsEventFromTemplate("nhs-number-field-not-present.xml", randomNhsNumber(), randomNemsMessageId());
 //        NemsEventMessage nhsNumberVerificationFieldNotPresent = createNemsEventFromTemplate("nhs-number-verification-field-not-present.xml", randomNhsNumber(), randomNemsMessageId());
 //        NemsEventMessage episodeOfCareFieldNotPresent = createNemsEventFromTemplate("no-finished-episode-of-care.xml", randomNhsNumber(), randomNemsMessageId());
@@ -32,7 +36,7 @@ public class NemsEventFactory {
 //        NemsEventMessage odsCodeForFinishedPractiseNotPresent = createNemsEventFromTemplate("no-ods-code-for-finished-practise.xml", randomNhsNumber(), randomNemsMessageId());
 //        NemsEventMessage odsCodeIdentifierForManagingOrganizationNotPresent = createNemsEventFromTemplate("no-ods-code-identifier-for-managing-organization.xml", randomNhsNumber(), randomNemsMessageId());
 //        NemsEventMessage referenceForManagingOrganizationNotPresent = createNemsEventFromTemplate("no-reference-for-managing-organization.xml", randomNhsNumber(), randomNemsMessageId());
-        NemsEventMessage uriForManagingOrganizationNotPresent = createNemsEventFromTemplate("no-reference-for-uri-for-managing-organization.xml", randomNhsNumber(), randomNemsMessageId());
+        NemsEventMessage uriForManagingOrganizationNotPresent = createNemsEventFromTemplate("no-reference-for-uri-for-managing-organization.xml", randomNhsNumber(), randomNemsMessageId(),now.toString());
         Map<String, NemsEventMessage> messages = new HashMap<>();
         messages.put("nhsNumberVerification", nhsNumberVerification);//p
 //        messages.put("nhsNumberFieldNotPresent", nhsNumberFieldNotPresent);//p
