c9c8eb9147dbc72268603a016774191db56454fd
Jack Slater <jack.slater1@nhs.net>
2022-03-04 12:44:51 +0000

    [PRMT-2484] allow mesh client to handle rsa private keys

--- src/test/java/uk/nhs/prm/deduction/e2e/client/StackOverflowInsecureSSLContextLoader.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/client/StackOverflowInsecureSSLContextLoader.java
@@ -1,11 +1,16 @@
 package uk.nhs.prm.deduction.e2e.client;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+
 import javax.net.ssl.*;
 import javax.xml.bind.DatatypeConverter;
 import java.io.ByteArrayInputStream;
-import java.security.KeyFactory;
-import java.security.KeyStore;
-import java.security.NoSuchAlgorithmException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
@@ -24,10 +29,9 @@ public class StackOverflowInsecureSSLContextLoader {
         try {
             sslContext = SSLContext.getInstance("TLS");
             byte[] certDerBytes = parseDERFromPEM(clientCertInPemFormat.getBytes(), "-----BEGIN CERTIFICATE-----", "-----END CERTIFICATE-----");
-            byte[] keyDerBytes = parseDERFromPEM(clientKeyInPemFormat.getBytes(), "-----BEGIN PRIVATE KEY-----", "-----END PRIVATE KEY-----");
 
             X509Certificate cert = generateCertificateFromDER(certDerBytes);
-            RSAPrivateKey key = generatePrivateKeyFromDER(keyDerBytes);
+            RSAPrivateKey key = getRsaPrivateKey(clientKeyInPemFormat);
 
             KeyStore keystore = KeyStore.getInstance("JKS");
             keystore.load(null);
@@ -63,11 +67,29 @@ public class StackOverflowInsecureSSLContextLoader {
         return sslContext;
     }
 
+    private RSAPrivateKey getRsaPrivateKey(String clientKeyInPemFormat) throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
+        if (clientKeyInPemFormat.startsWith("-----BEGIN RSA PRIVATE KEY-----")) {
+            return getPrivateKeyForRsaKeyString(clientKeyInPemFormat);
+        } else {
+            byte[] keyDerBytes = parseDERFromPEM(clientKeyInPemFormat.getBytes(), "-----BEGIN PRIVATE KEY-----", "-----END PRIVATE KEY-----");
+            return generatePrivateKeyFromDER(keyDerBytes);
+        }
+    }
+
     private static byte[] parseDERFromPEM(byte[] pem, String beginDelimiter, String endDelimiter) {
         String data = new String(pem);
         String[] tokens = data.split(beginDelimiter);
         tokens = tokens[1].split(endDelimiter);
         return DatatypeConverter.parseBase64Binary(tokens[0]);
+    }
+
+    private RSAPrivateKey getPrivateKeyForRsaKeyString(String privateKeyString) throws IOException {
+        PEMParser pemParser = new PEMParser(new StringReader(privateKeyString));
+        Security.addProvider(new BouncyCastleProvider());
+        JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+        Object object = pemParser.readObject();
+        KeyPair kp = converter.getKeyPair((PEMKeyPair) object);
+        return (RSAPrivateKey) kp.getPrivate();
     }
 
     private static RSAPrivateKey generatePrivateKeyFromDER(byte[] keyBytes) throws InvalidKeySpecException, NoSuchAlgorithmException {
