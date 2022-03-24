87e5729c526301dc3a24149209653e10941b77c2
Dan Moore <dan.moore1@nhs.net>
2022-03-24 14:29:38 +0000

    [PRMT-2476] Add passthrough of nems message id to day 2 live test

--- src/test/java/uk/nhs/prm/deduction/e2e/live_technical_test/day2/ValidateMOFUpdatedTest.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/live_technical_test/day2/ValidateMOFUpdatedTest.java
@@ -1,9 +1,11 @@
-package uk.nhs.prm.deduction.e2e.live.day2;
+package uk.nhs.prm.deduction.e2e.live_technical_test.day2;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import uk.nhs.prm.deduction.e2e.live_technical_test.TestParameters;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -12,7 +14,7 @@ public class ValidateMOFUpdatedTest {
 
     @Test
     public void shouldMoveSingleSuspensionMessageFromMeshMailBoxToNemsIncomingQueue() {
-       Assertions.assertThat(true);
+       assertThat(TestParameters.fetchTestParameter("LIVE_TECHNICAL_TEST_NEMS_MESSAGE_ID")).contains("-");
     }
 
 }
