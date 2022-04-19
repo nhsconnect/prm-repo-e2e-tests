434e702220ea88bf0aad83ffbd90b7106e248175
syedsadiqali <syed.ali69@nhs.net>
2022-04-19 14:53:17 +0100

    Added check for patient status on PDS for Day 1 test

--- src/test/java/uk/nhs/prm/deduction/e2e/live_technical_test/day1/ChangeOfGPMessageReceivedTest.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/live_technical_test/day1/ChangeOfGPMessageReceivedTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import uk.nhs.prm.deduction.e2e.TestConfiguration;
+import uk.nhs.prm.deduction.e2e.pdsadaptor.PdsAdaptorClient;
+import uk.nhs.prm.deduction.e2e.pdsadaptor.PdsAdaptorResponse;
 import uk.nhs.prm.deduction.e2e.performance.awsauth.AssumeRoleCredentialsProviderFactory;
 import uk.nhs.prm.deduction.e2e.performance.awsauth.AutoRefreshingRoleAssumingSqsClient;
 import uk.nhs.prm.deduction.e2e.queue.SqsQueue;
@@ -18,17 +20,26 @@ import static uk.nhs.prm.deduction.e2e.live_technical_test.TestParameters.output
 public class ChangeOfGPMessageReceivedTest {
 
     private SuspensionMessageRealQueue suspensionMessageRealQueue;
+    private TestConfiguration config = new TestConfiguration();
 
     @BeforeEach
     public void setUp() {
         var sqsClient = new AutoRefreshingRoleAssumingSqsClient(new AssumeRoleCredentialsProviderFactory());
-        suspensionMessageRealQueue = new SuspensionMessageRealQueue(new SqsQueue(sqsClient), new TestConfiguration());
+        suspensionMessageRealQueue = new SuspensionMessageRealQueue(new SqsQueue(sqsClient), config);
     }
 
     @Test
     public void shouldHaveReceivedSingleSuspensionChangeOfGpMessageRelatedToTestPatient() {
         var testPatientNhsNumber = fetchTestParameter("LIVE_TECHNICAL_TEST_NHS_NUMBER");
         var testPatientPreviousGp = fetchTestParameter("LIVE_TECHNICAL_TEST_PREVIOUS_GP");
+
+        System.out.println("Checking if nhs number is synthetic");
+
+        assertThat(testPatientNhsNumber).startsWith(config.getSyntheticPatientPrefix());
+
+        var pdsResponse = getPatientStatusOnPDSForSyntheticPatient(testPatientNhsNumber);
+
+        assertThat(pdsResponse.getIsSuspended()).isTrue();
 
         System.out.println("expecting test nhs number and previous gp of: " + testPatientNhsNumber + ", " + testPatientPreviousGp);
 
@@ -38,6 +49,19 @@ public class ChangeOfGPMessageReceivedTest {
         outputTestParameter("live_technical_test_nems_message_id", suspensionMessage.nemsMessageId());
 
         assertThat(suspensionMessage.previousGp()).isEqualTo(testPatientPreviousGp);
+
+    }
+
+    private PdsAdaptorResponse getPatientStatusOnPDSForSyntheticPatient(String testPatientNhsNumber) {
+        String pdsAdaptorUsernameXXX = "live-test";
+        return fetchPdsPatientStatus(pdsAdaptorUsernameXXX, testPatientNhsNumber);
+    }
+
+    private PdsAdaptorResponse fetchPdsPatientStatus(String pdsAdaptorUsername, String testPatientNhsNumber) {
+
+        var pds = new PdsAdaptorClient(pdsAdaptorUsername, config.getPdsAdaptorLiveTestApiKey(), config.getPdsAdaptorUrl());
+
+        return pds.getSuspendedPatientStatus(testPatientNhsNumber);
     }
 
 }
