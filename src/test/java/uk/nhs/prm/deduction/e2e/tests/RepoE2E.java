d370401263326362dfae6de7480962550171c1ff
Rosie Amphlett <rosie.amphlett1@nhs.net>
2022-06-06 14:18:11 +0100

    [PRMT-2656] Added nems event last updated value to put on repo incoming queue

--- src/test/java/uk/nhs/prm/deduction/e2e/tests/RepoE2E.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/tests/RepoE2E.java
@@ -83,7 +83,7 @@ public class RepoE2E {
     }
 
     private void postMessageToRepoIncomingQueue(String nemsMessageId, String conversationId, String nhsNumber) {
-        String message = "{\"nhsNumber\":\"" + nhsNumber + "\",\"nemsMessageId\":\"" + nemsMessageId + "\", \"sourceGp\":\"N82668\",\"destinationGp\":\"B85002\",\"conversationId\":\"" + conversationId + "\"}";
+        String message = "{\"nhsNumber\":\"" + nhsNumber + "\",\"nemsMessageId\":\"" + nemsMessageId + "\", \"nemsEventLastUpdated\":\"last-updated\",\"sourceGp\":\"N82668\",\"destinationGp\":\"B85002\",\"conversationId\":\"" + conversationId + "\"}";
         repoIncomingQueue.postAMessage(message);
         System.out.println("Sent message to repo incoming queue");
     }
