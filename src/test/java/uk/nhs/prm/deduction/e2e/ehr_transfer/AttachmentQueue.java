9294fb8d15d162d2468dcc2e96433b573cc50175
syedsadiqali <syed.ali69@nhs.net>
2022-06-14 10:04:47 +0100

    [PRMT-2650]- Fixed the merge issue from previous two commits

---
+++ src/test/java/uk/nhs/prm/deduction/e2e/ehr_transfer/AttachmentQueue.java
@@ -0,0 +1,16 @@
+package uk.nhs.prm.deduction.e2e.ehr_transfer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.nhs.prm.deduction.e2e.TestConfiguration;
+import uk.nhs.prm.deduction.e2e.queue.QueueMessageHelper;
+import uk.nhs.prm.deduction.e2e.queue.SqsQueue;
+
+@Component
+public class AttachmentQueue extends QueueMessageHelper {
+
+    @Autowired
+    public AttachmentQueue(SqsQueue sqsQueue, TestConfiguration configuration) {
+        super(sqsQueue, configuration.attachmentQueueUri());
+    }
+}
