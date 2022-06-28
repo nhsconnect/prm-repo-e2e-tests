d74d14eb017a58d66e5cd6fd8b3dc81235109e62
syedsadiqali <syed.ali69@nhs.net>
2022-06-29 00:25:22 +0100

    Revert to previous commit

--- src/test/java/uk/nhs/prm/deduction/e2e/models/Gp2GpSystem.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/models/Gp2GpSystem.java
@@ -3,12 +3,26 @@ package uk.nhs.prm.deduction.e2e.models;
 import uk.nhs.prm.deduction.e2e.TestConfiguration;
 
 public enum Gp2GpSystem {
-    TPP_PTL_INT("M85019"), REPO_DEV("B85002"), REPO_TEST("B86041"), EMIS_PTL_INT("N82668");
+    TPP_PTL_INT("M85019"),
+    REPO_DEV("B85002"),
+    REPO_TEST("B86041"),
+    EMIS_PTL_INT("N82668");
 
     private String odsCode;
 
     Gp2GpSystem(String odsCode) {
         this.odsCode = odsCode;
+    }
+
+    public static Gp2GpSystem repoInEnv(TestConfiguration config) {
+        var environmentName = config.getEnvironmentName();
+        if ("dev".equals(environmentName)) {
+            return REPO_DEV;
+        }
+        if ("test".equals(environmentName)) {
+            return REPO_TEST;
+        }
+        throw new IllegalStateException("Don't know about repo in environment: " + environmentName);
     }
 
     public String odsCode() {
