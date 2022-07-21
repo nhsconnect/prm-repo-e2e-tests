08f61c492ee0d7a1fa4413265c5fba5307819c67
Dan Moore <dan.moore1@nhs.net>
2022-07-21 17:04:45 +0100

    WIP large ehr nhs numbers

--- src/test/java/uk/nhs/prm/deduction/e2e/tests/LargeEhrVariant.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/tests/LargeEhrVariant.java
@@ -2,6 +2,8 @@ package uk.nhs.prm.deduction.e2e.tests;
 
 public enum LargeEhrVariant {
     SINGLE_ATTACHMENT(Patient.WITH_SINGLE_ATTACHMENT_LARGE_EHR);
+ SUPER_LARGE(new Patient("9693796004"))
+ HIGH_ATTACHMENT_COUNT(new Patient("9693796179"))
 
     private Patient patient;
 
