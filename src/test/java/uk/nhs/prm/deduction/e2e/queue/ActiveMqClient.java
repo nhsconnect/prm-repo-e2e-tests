e549829cc405b5046fde0b3aabcc01f44fc61157
Mario Giampietri <mario.giampietri1@nhs.net>
2022-08-05 11:39:11 +0100

    [PRMT-2684] split cases of unprocessable message body and unprocessable ehr message (ParsedMessage class) in repo tests
    Still working on the second one, currently disabled

--- src/test/java/uk/nhs/prm/deduction/e2e/queue/ActiveMqClient.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/queue/ActiveMqClient.java
@@ -6,6 +6,7 @@ import org.apache.activemq.pool.PooledConnectionFactory;
 import uk.nhs.prm.deduction.e2e.TestConfiguration;
 
 import javax.jms.*;
+import java.nio.charset.StandardCharsets;
 
 public class ActiveMqClient {
     TestConfiguration config;
@@ -15,7 +16,7 @@ public class ActiveMqClient {
 
     String randomOption = "?randomize=false";
 
-    public void postAMessageToAQueue(String queueName, String message) throws JMSException {
+    private Session getSession() throws JMSException {
         String url =  String.format("failover:(%s,%s)%s", config.getActiveMqEndpoint0(),config.getActiveMqEndpoint1(), randomOption);
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
         connectionFactory.setUserName(config.getMqUserName());
@@ -28,13 +29,33 @@ public class ActiveMqClient {
         producerConnection.start();
 
         final Session producerSession = producerConnection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        return producerSession;
+    }
 
+    private MessageProducer getMessageProducer(Session producerSession, String queueName) throws JMSException {
         final Destination producerDestination = producerSession.createQueue(queueName);
-
         final MessageProducer producer = producerSession.createProducer(producerDestination);
         producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+        return producer;
+    }
 
-        TextMessage producerMessage = producerSession.createTextMessage(message);
+    public void postBrokenMessageToAQueue(String queueName, String message) throws JMSException {
+        final Session producerSession = getSession();
+        final MessageProducer producer = getMessageProducer(producerSession, queueName);
+        var producerMessage = producerSession.createTextMessage(message);
+
+        producer.send(producerMessage);
+        System.out.println("Message sent.");
+    }
+
+    public void postMessageToAQueue(String queueName, String message) throws JMSException {
+        final Session producerSession = getSession();
+        final MessageProducer producer = getMessageProducer(producerSession, queueName);
+
+        var producerMessage = producerSession.createBytesMessage();
+        var bytesArray = message.getBytes(StandardCharsets.UTF_8);
+        producerMessage.writeBytes(bytesArray);
+        producerMessage.reset();
 
         producer.send(producerMessage);
         System.out.println("Message sent.");
