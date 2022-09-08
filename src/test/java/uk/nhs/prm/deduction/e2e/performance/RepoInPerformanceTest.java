a9bced3551a3d7f35da26ffaba4de4bb2c263078
Mario Giampietri <mario.giampietri1@nhs.net>
2022-09-08 17:17:18 +0100

    [PRMT-2579] repo-in perf test: polling from complete transfer queue in a more efficient way

--- src/test/java/uk/nhs/prm/deduction/e2e/performance/RepoInPerformanceTest.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/performance/RepoInPerformanceTest.java
@@ -15,6 +15,7 @@ import uk.nhs.prm.deduction.e2e.models.RepoIncomingMessage;
 import uk.nhs.prm.deduction.e2e.models.RepoIncomingMessageBuilder;
 import uk.nhs.prm.deduction.e2e.performance.awsauth.AssumeRoleCredentialsProviderFactory;
 import uk.nhs.prm.deduction.e2e.performance.awsauth.AutoRefreshingRoleAssumingSqsClient;
+import uk.nhs.prm.deduction.e2e.queue.SqsMessage;
 import uk.nhs.prm.deduction.e2e.queue.SqsQueue;
 import uk.nhs.prm.deduction.e2e.queue.activemq.ForceXercesParserSoLogbackDoesNotBlowUpWhenUsingSwiftMqClient;
 import uk.nhs.prm.deduction.e2e.queue.activemq.SimpleAmqpQueue;
@@ -25,6 +26,7 @@ import uk.nhs.prm.deduction.e2e.utility.Resources;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+import static java.time.LocalDateTime.now;
 import static java.util.UUID.randomUUID;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -62,38 +64,50 @@ public class RepoInPerformanceTest {
 
     @Test
     public void trackBehaviourOfHighNumberOfMessagesSentToEhrTransferService() {
-        var numberOfRecordToBeProcessed = 2;
-        var repoIncomingMessages = new ArrayList<RepoIncomingMessage>();
+        var numberOfRecordToBeProcessed = 1000;
+        var messagesToBeProcessed = new ArrayList<RepoIncomingMessage>();
 
         for (int i = 0; i < numberOfRecordToBeProcessed ; i++) {
             var message = new RepoIncomingMessageBuilder()
                     .withNhsNumber(TestData.generateRandomNhsNumber())
                     .withEhrSourceGp(Gp2GpSystem.EMIS_PTL_INT)
                     .build();
-            repoIncomingMessages.add(message);
+            messagesToBeProcessed.add(message);
         }
 
-        repoIncomingMessages.forEach(message -> repoIncomingQueue.send(message));
+        messagesToBeProcessed.forEach(message -> repoIncomingQueue.send(message));
 
         // TODO: to be fixed in perf env (ok in dev)
-        System.out.println("Ensuring records are stored in tracker db...");
-        repoIncomingMessages.forEach(message ->
-            assertTrue(trackerDb.conversationIdExists(message.conversationId()))
-        );
+//        System.out.println("Ensuring records are stored in tracker db...");
+//        messagesToBeProcessed.forEach(message ->
+//            assertTrue(trackerDb.conversationIdExists(message.conversationId()))
+//        );
 
         System.out.println("DB setup completed. About to send messages to mq...");
         var inboundQueueFromMhs = new SimpleAmqpQueue(config);
 
-        repoIncomingMessages.forEach(message -> {
+        messagesToBeProcessed.forEach(message -> {
             var conversationId = message.conversationId();
             var smallEhr = getSmallMessageWithUniqueConversationIdAndMessageId(conversationId);
             inboundQueueFromMhs.sendMessage(smallEhr, conversationId);
         });
         inboundQueueFromMhs.close();
 
-        repoIncomingMessages.forEach(message ->
-            assertThat(transferCompleteQueue.getMessageContainingAttribute("conversationId", message.conversationId(), 5, TimeUnit.MINUTES))
-        );
+        var timeout = now().plusMinutes(10);
+        while (now().isBefore(timeout) && messagesToBeProcessed.size() > 0) {
+            for (SqsMessage nextMessage : transferCompleteQueue.getNextMessages(timeout)) {
+                var conversationId = nextMessage.attributes().get("conversationId").stringValue();
+                messagesToBeProcessed.removeIf(message -> {
+                    if (message.conversationId().equals(conversationId)) {
+                        System.out.println("Found in transfer complete queue message with conversationId " + conversationId);
+                        return true;
+                    }
+                    return false;
+                });
+            }
+        }
+
+        assertTrue(messagesToBeProcessed.isEmpty());
     }
 
     private String getSmallMessageWithUniqueConversationIdAndMessageId(String conversationId) {
