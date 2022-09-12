6cd8782ee5dfd5bccc6cf48e2fa0af56dd66c475
syedsadiqali <syed.ali69@nhs.net>
2022-09-12 11:38:51 +0100

    [PRMT-2765] Fixed re-registration test
    Co-authored-by: Shruthi Chandekar <shruthi.chandekar1@nhs.net>

--- src/test/java/uk/nhs/prm/deduction/e2e/active_suspensions_db/ActiveSuspensionsDB.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/active_suspensions_db/ActiveSuspensionsDB.java
@@ -1,6 +1,7 @@
 package uk.nhs.prm.deduction.e2e.active_suspensions_db;
 
 import uk.nhs.prm.deduction.e2e.reregistration.active_suspensions_db.ActiveSuspensionsDbClient;
+import uk.nhs.prm.deduction.e2e.reregistration.models.ActiveSuspensionsMessage;
 
 public class ActiveSuspensionsDB {
     ActiveSuspensionsDbClient activeSuspensionsDbClient;
@@ -15,5 +16,9 @@ public class ActiveSuspensionsDB {
             return true;
         }
         return false;
+    }
+    public void save(ActiveSuspensionsMessage message)
+    {
+        activeSuspensionsDbClient.save(message);
     }
 }
