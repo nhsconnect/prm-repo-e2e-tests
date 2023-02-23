2f2c983cb235e3c014184402fc92761c7ea1deb0
Dan Moore <dan.moore1@nhs.net>
2023-02-23 12:07:38 +0000

    [PRMT-3168] Split suspended check from ehr-in for synth test
    Renames for clarity
    
    Co-authored-by: syedsadiqali <syed.ali69@nhs.net>

--- src/test/java/uk/nhs/prm/deduction/e2e/live_technical_test/repo/ValidateSyntheticEhrTransferInToRepoUsingMofTest.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/live_technical_test/repo/ValidateSyntheticEhrTransferInToRepoUsingMofTest.java
@@ -3,45 +3,38 @@ package uk.nhs.prm.deduction.e2e.live_technical_test.repo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
 import uk.nhs.prm.deduction.e2e.TestConfiguration;
 import uk.nhs.prm.deduction.e2e.ehr_transfer.RepoIncomingQueue;
 import uk.nhs.prm.deduction.e2e.live_technical_test.helpers.TestPatientValidator;
-import uk.nhs.prm.deduction.e2e.models.Gp2GpSystem;
 import uk.nhs.prm.deduction.e2e.models.RepoIncomingMessageBuilder;
+import uk.nhs.prm.deduction.e2e.pdsadaptor.PdsAdaptorClient;
+import uk.nhs.prm.deduction.e2e.pdsadaptor.PdsAdaptorResponse;
 import uk.nhs.prm.deduction.e2e.performance.awsauth.AssumeRoleCredentialsProviderFactory;
 import uk.nhs.prm.deduction.e2e.performance.awsauth.AutoRefreshingRoleAssumingSqsClient;
 import uk.nhs.prm.deduction.e2e.queue.ThinlyWrappedSqsClient;
 import uk.nhs.prm.deduction.e2e.services.ehr_repo.EhrRepoClient;
-import uk.nhs.prm.deduction.e2e.services.gp2gp_messenger.Gp2GpMessengerClient;
-import uk.nhs.prm.deduction.e2e.pdsadaptor.PdsAdaptorClient;
-import uk.nhs.prm.deduction.e2e.pdsadaptor.PdsAdaptorResponse;
-import uk.nhs.prm.deduction.e2e.transfer_tracker_db.TrackerDb;
 
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static uk.nhs.prm.deduction.e2e.live_technical_test.TestParameters.fetchTestParameter;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class ValidateEhrTransferToRepoUsingMofTest {
+public class ValidateSyntheticEhrTransferInToRepoUsingMofTest {
 
     private final TestConfiguration config = new TestConfiguration();
     private static final String PDS_ADAPTOR_TEST_USERNAME = "live-test";
     private PdsAdaptorClient pdsAdaptorClient;
-    private Gp2GpMessengerClient gp2GpMessengerClient;
     private EhrRepoClient ehrRepoClient;
     private TestPatientValidator patientValidator = new TestPatientValidator();
     private RepoIncomingQueue repoIncomingQueue;
+
     @BeforeEach
     void setUp() {
         var sqsClient = new AutoRefreshingRoleAssumingSqsClient(new AssumeRoleCredentialsProviderFactory());
         pdsAdaptorClient = new PdsAdaptorClient(PDS_ADAPTOR_TEST_USERNAME, config.getPdsAdaptorLiveTestApiKey(), config.getPdsAdaptorUrl());
-        gp2GpMessengerClient = new Gp2GpMessengerClient(config.getGp2GpMessengerApiKey(), config.getGp2GpMessengerUrl());
         ehrRepoClient = new EhrRepoClient(config.getEhrRepoApiKey(), config.getEhrRepoUrl());
         repoIncomingQueue = new RepoIncomingQueue(new ThinlyWrappedSqsClient(sqsClient), config);
     }
@@ -88,13 +81,7 @@ public class ValidateEhrTransferToRepoUsingMofTest {
         return pdsResponse;
     }
 
-    private void isHealthRecordRequestSuccessful(String testPatientNhsNumber, String testPatientPreviousGp, String conversationId) {
-        var healthRecordRequestSentSuccessful = gp2GpMessengerClient.isHealthRecordRequestSentSuccessful(testPatientNhsNumber, config.getRepoOdsCode(), config.getRepoAsid(),
-                testPatientPreviousGp, conversationId);
-        assertThat(healthRecordRequestSentSuccessful).isTrue();
-    }
     private boolean isSafeListedOrSynthetic(String testPatientNhsNumber) {
         return patientValidator.isIncludedInTheTest(testPatientNhsNumber, config.getSafeListedPatientList(), config.getSyntheticPatientPrefix());
     }
-
 }
