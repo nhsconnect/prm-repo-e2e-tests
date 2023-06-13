e548cd8f841ac109556d75ef761c3a16cb496295
Joe Fong <joe.fong1@nhs.net>
2023-06-13 14:41:17 +0100

    [PRMT-3413] Allow health check to wait for 2xx response for at most 5 sec, unify the way to generate trigger message in two tests

--- src/test/java/uk/nhs/prm/deduction/e2e/utility/HealthCheck.java
+++ src/test/java/uk/nhs/prm/deduction/e2e/utility/HealthCheck.java
@@ -1,23 +1,34 @@
 package uk.nhs.prm.deduction.e2e.utility;
 
 import org.springframework.http.*;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.is;
 
 
 public class HealthCheck {
     private static final RestTemplate restTemplate = new RestTemplate();
     public static boolean isHealthCheckPassing(String rootUrl) {
         String healthCheckUrl = rootUrl + "health";
+        System.out.printf("checking service health status of rootUrl: %s" , rootUrl);
+
         try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            ResponseEntity<String> exchange = restTemplate.exchange(healthCheckUrl, HttpMethod.GET, new HttpEntity<String>(headers), String.class);
-            System.out.printf("checking service health status of rootUrl: %s" , rootUrl);
-            return exchange.getStatusCode().is2xxSuccessful();
-        } catch (HttpStatusCodeException e) {
-            System.out.printf("Error retrieving health check status from %s. Status code: %s. Error: %s%n", rootUrl, e.getStatusCode(), e.getMessage());
+            return await().atMost(5, TimeUnit.SECONDS)
+                    .with()
+                    .pollInterval(200, TimeUnit.MILLISECONDS)
+                    .until(() -> getRequest(healthCheckUrl).getStatusCode().is2xxSuccessful(), is(true));
+        } catch (Exception e) {
+            System.out.printf("Error retrieving health check status from %s. Error: %s%n", rootUrl, e.getMessage());
             return false;
         }
+    }
+
+    private static ResponseEntity<String> getRequest(String url) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<String>(headers), String.class);
     }
 }
