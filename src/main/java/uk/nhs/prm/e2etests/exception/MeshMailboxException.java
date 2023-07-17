6276cd7d9f6fda3a6a590416c729ce14cf478e16
martin-nhs <martin.taylor22@nhs.net>
2023-07-17 12:07:23 +0100

    [PRMT-3486] Addressed Typo in MeshMailboxException.

--- src/main/java/uk/nhs/prm/e2etests/exception/MeshMailboxException.java
+++ src/main/java/uk/nhs/prm/e2etests/exception/MeshMailboxException.java
@@ -1,7 +1,7 @@
 package uk.nhs.prm.e2etests.exception;
 
 public class MeshMailboxException extends RuntimeException {
-    private static final String MESSAGE = "An exception occurred while interacting with the Mesh Mailox, details: %s";
+    private static final String MESSAGE = "An exception occurred while interacting with the Mesh Mailbox, details: %s";
 
     public MeshMailboxException(String details) {
         super(String.format(MESSAGE, details));
