f8b327a3fee7f9b5778add353d5137f3106acc77
Mohammad Iqbal <mohammad.iqbal27@nhs.net>
2023-07-27 16:38:46 +0100

    [PRMT-3487] Addressed Pull Request Comments and reverted some changes

--- src/main/java/uk/nhs/prm/e2etests/timing/Timer.java
+++ src/main/java/uk/nhs/prm/e2etests/timing/Timer.java
@@ -1,6 +1,6 @@
 package uk.nhs.prm.e2etests.timing;
 
-// TODO PRMT-3487: CAN'T WE JUST SLAP IT AS A PUBLIC FINAL CLASS
+// TODO PRMT-3523: CAN'T WE JUST SLAP IT AS A PUBLIC FINAL CLASS
 // AND HAVE THE milliseconds BE STATIC AND MOVE IT TO
 // THE UTILITY DIR -> proceed to statically import??
 
