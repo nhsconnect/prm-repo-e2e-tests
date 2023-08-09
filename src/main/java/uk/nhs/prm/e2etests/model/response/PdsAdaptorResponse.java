7c4d9615d10f5b451955a18b173afe8d8b83f49f
martin-nhs <martin.taylor22@nhs.net>
2023-08-09 11:33:51 +0100

    [PRMT-3574] Addressed PR comment https://github.com/nhsconnect/prm-repo-e2e-tests/pull/53#discussion_r1288262925

--- src/main/java/uk/nhs/prm/e2etests/model/response/PdsAdaptorResponse.java
+++ src/main/java/uk/nhs/prm/e2etests/model/response/PdsAdaptorResponse.java
@@ -1,12 +1,12 @@
 package uk.nhs.prm.e2etests.model.response;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.Objects;
-
 @Getter
+@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 public class PdsAdaptorResponse {
@@ -14,17 +14,4 @@ public class PdsAdaptorResponse {
     private String currentOdsCode;
     private String managingOrganisation;
     private String recordETag;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PdsAdaptorResponse that = (PdsAdaptorResponse) o;
-        return Objects.equals(isSuspended, that.isSuspended) && Objects.equals(currentOdsCode, that.currentOdsCode) && Objects.equals(managingOrganisation, that.managingOrganisation) && Objects.equals(recordETag, that.recordETag);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(isSuspended, currentOdsCode, managingOrganisation, recordETag);
-    }
 }
