04b75b8003a79390cf4ff32a8402650f11d0df6c
martin-nhs <martin.taylor22@nhs.net>
2023-08-09 11:32:39 +0100

    [PRMT-3574] Addressed PR comment https://github.com/nhsconnect/prm-repo-e2e-tests/pull/53#discussion_r1288260740

--- src/main/java/uk/nhs/prm/e2etests/model/request/PdsAdaptorRequest.java
+++ src/main/java/uk/nhs/prm/e2etests/model/request/PdsAdaptorRequest.java
@@ -1,19 +1,8 @@
 package uk.nhs.prm.e2etests.model.request;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = false)
 public record PdsAdaptorRequest(String previousGp,
-                                String recordETag) {
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PdsAdaptorRequest that = (PdsAdaptorRequest) o;
-        return Objects.equals(previousGp, that.previousGp) && Objects.equals(recordETag, that.recordETag);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(previousGp, recordETag);
-    }
-}
+                                String recordETag)
+{ }
