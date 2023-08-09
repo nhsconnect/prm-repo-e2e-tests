86fe58d1eafdf02fe023cac4a4d3b8e1d99c606c
martin-nhs <martin.taylor22@nhs.net>
2023-08-09 10:47:33 +0100

    [PRMT-3574] Addressed PR comment https://github.com/nhsconnect/prm-repo-e2e-tests/pull/53#discussion_r1288224150

--- src/main/java/uk/nhs/prm/e2etests/service/PdsAdaptorService.java
+++ src/main/java/uk/nhs/prm/e2etests/service/PdsAdaptorService.java
@@ -42,7 +42,7 @@ public class PdsAdaptorService {
         String patientUrl = buildUrl(patientRootUrl, nhsNumber);
         log.info("Requesting patient status from pds adaptor: {}", patientRootUrl);
         ResponseEntity<PdsAdaptorResponse> response =
-                restTemplate.exchange(patientUrl, HttpMethod.GET, new HttpEntity<>(createHeaders(username, apiKey)), PdsAdaptorResponse.class);
+            restTemplate.exchange(patientUrl, HttpMethod.GET, new HttpEntity<>(createHeaders(username, apiKey)), PdsAdaptorResponse.class);
         log.info("Response received from pds adaptor: {}", response.getBody());
 
         return response.getBody();
