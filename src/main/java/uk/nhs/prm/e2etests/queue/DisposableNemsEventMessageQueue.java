b040c0a59a74fb5de255e2745f4958e171788d22
Carl Smith <carl.smith21@nhs.net>
2024-02-14 13:59:46 +0000

    move queue provisoning logic into abstract classes

---
+++ src/main/java/uk/nhs/prm/e2etests/queue/DisposableNemsEventMessageQueue.java
@@ -0,0 +1,47 @@
+package uk.nhs.prm.e2etests.queue;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.nhs.prm.e2etests.service.SnsService;
+import uk.nhs.prm.e2etests.service.SqsService;
+
+@Component
+public abstract class DisposableNemsEventMessageQueue extends AbstractNemsEventMessageQueue {
+    protected final SqsService sqsService;
+    protected final SnsService snsService;
+    protected final String queueName;
+    protected final String queueArn;
+    protected final String queueUrl;
+    protected final String topicArn;
+    @Autowired
+    public DisposableNemsEventMessageQueue(
+            SqsService sqsService,
+            SnsService snsService,
+            String queueName,
+            String queueArn,
+            String queueUrl,
+            String topicArn) {
+        super(sqsService, queueUrl);
+        this.sqsService = sqsService;
+        this.snsService = snsService;
+        this.queueName = queueName;
+        this.queueArn = queueArn;
+        this.queueUrl = queueUrl;
+        this.topicArn = topicArn;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.sqsService.createQueue(this.queueName);
+        this.snsService.subscribeQueueToTopic(
+                this.queueArn,
+                this.topicArn);
+    }
+
+    @PreDestroy
+    public void cleanUp() {
+        this.sqsService.deleteQueue(this.queueUrl);
+    }
+}
--- src/main/java/uk/nhs/prm/e2etests/queue/nems/observability/NemsEventProcessorReRegistrationsOQ.java
+++ src/main/java/uk/nhs/prm/e2etests/queue/nems/observability/NemsEventProcessorReRegistrationsOQ.java
@@ -1,44 +1,28 @@
 package uk.nhs.prm.e2etests.queue.nems.observability;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.nhs.prm.e2etests.property.TopicProperties;
-import uk.nhs.prm.e2etests.queue.AbstractNemsEventMessageQueue;
 import uk.nhs.prm.e2etests.property.QueueProperties;
 import org.springframework.stereotype.Component;
+import uk.nhs.prm.e2etests.queue.DisposableNemsEventMessageQueue;
 import uk.nhs.prm.e2etests.service.SnsService;
 import uk.nhs.prm.e2etests.service.SqsService;
 
 @Component
-public class NemsEventProcessorReRegistrationsOQ extends AbstractNemsEventMessageQueue {
-    protected final SqsService sqsService;
-    protected final SnsService snsService;
-    protected final QueueProperties queueProperties;
-    protected final TopicProperties topicProperties;
+public class NemsEventProcessorReRegistrationsOQ extends DisposableNemsEventMessageQueue {
     @Autowired
     public NemsEventProcessorReRegistrationsOQ(
             SqsService sqsService,
             SnsService snsService,
             QueueProperties queueProperties,
             TopicProperties topicProperties) {
-        super(sqsService, queueProperties.getNemsEventProcessorReregistrationObservabilityQueueUrl());
-        this.sqsService = sqsService;
-        this.snsService = snsService;
-        this.queueProperties = queueProperties;
-        this.topicProperties = topicProperties;
-    }
-
-    @PostConstruct
-    public void init() {
-        this.sqsService.createQueue(this.queueProperties.getNemsEventProcessorReregistrationObservabilityQueueName());
-        this.snsService.subscribeQueueToTopic(
-                this.queueProperties.getNemsEventProcessorReregistrationObservabilityQueueArn(),
-                this.topicProperties.getNemsEventProcessorRegistrationTopicArn());
-    }
-
-    @PreDestroy
-    public void cleanUp() {
-        this.sqsService.deleteQueue(this.queueProperties.getNemsEventProcessorReregistrationObservabilityQueueUrl());
+        super(
+                sqsService,
+                snsService,
+                queueProperties.getNemsEventProcessorReregistrationObservabilityQueueName(),
+                queueProperties.getNemsEventProcessorReregistrationObservabilityQueueArn(),
+                queueProperties.getNemsEventProcessorReregistrationObservabilityQueueUrl(),
+                topicProperties.getNemsEventProcessorRegistrationTopicArn()
+        );
     }
 }
