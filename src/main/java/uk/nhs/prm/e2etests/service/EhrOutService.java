46921b6605bba894ca7b9b192f73cd16489edf1c
Andy Flint <andrew.flint@answerdigital.com>
2024-03-25 12:53:00 +0000

    [PRMT-4672] Deprecated logic for ehr-out database

--- src/main/java/uk/nhs/prm/e2etests/service/EhrOutService.java
+++ src/main/java/uk/nhs/prm/e2etests/service/EhrOutService.java
@@ -8,6 +8,10 @@ import uk.nhs.prm.e2etests.repository.EhrOutDatabaseAcknowledgementRepository;
 
 import java.util.UUID;
 
+/**
+ * @deprecated PRMT-4670 old Transfer Tracker DB logic - to be removed
+ */
+@Deprecated(since="2.0.0", forRemoval = true)
 @Service
 @Log4j2
 public class EhrOutService {
