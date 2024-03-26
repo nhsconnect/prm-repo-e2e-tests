24696e2842e5c372bee20115900abb2f7ee9e32c
Andy Flint <andrew.flint@answerdigital.com>
2024-03-26 15:25:44 +0000

    [PRMT-4667] Created new TransferTrackerService and ConversationTransferStatus enum

---
+++ src/main/java/uk/nhs/prm/e2etests/enumeration/ConversationTransferStatus.java
@@ -0,0 +1,10 @@
+package uk.nhs.prm.e2etests.enumeration;
+
+public enum ConversationTransferStatus {
+    INBOUND_STARTED,
+    INBOUND_REQUEST_SENT,
+    INBOUND_CONTINUE_REQUEST_SENT,
+    INBOUND_COMPLETE,
+    INBOUND_FAILED,
+    INBOUND_TIMEOUT
+}
