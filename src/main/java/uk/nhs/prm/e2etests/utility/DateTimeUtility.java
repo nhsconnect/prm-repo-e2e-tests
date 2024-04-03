810391bf275a17c5e2812c4b7ad7de907ba5dd53
Andy Flint <andrew.flint@answerdigital.com>
2024-04-03 14:30:18 +0100

    [PRMT-4669] added entity mapping improvements for ConversationRecords

--- src/main/java/uk/nhs/prm/e2etests/utility/DateTimeUtility.java
+++ src/main/java/uk/nhs/prm/e2etests/utility/DateTimeUtility.java
@@ -7,13 +7,18 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class DateTimeUtility {
-
     private static final String ZONE_ID = "Europe/London";
 
     public static String getStartOfYearDateTimeAsString() {
         LocalDateTime startOfyear = LocalDateTime.of(2024, 1, 1, 1, 1, 1);
 
         return ZonedDateTime.of(startOfyear, ZoneId.of(ZONE_ID))
+                .truncatedTo(ChronoUnit.MINUTES)
+                .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+    }
+
+    public static String getIsoTimestampForString(String timestamp) {
+        return ZonedDateTime.of(LocalDateTime.parse(timestamp), ZoneId.of(ZONE_ID))
                 .truncatedTo(ChronoUnit.MINUTES)
                 .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
     }
