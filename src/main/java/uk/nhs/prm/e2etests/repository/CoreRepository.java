aa2cdcea36a0ec880d3b287c9ca0b5f471057e57
martin-nhs <martin.taylor22@nhs.net>
2024-04-18 15:27:37 +0100

    [PRMT-4747] Added CoreRecord.java and a method to update the deleted at field.

---
+++ src/main/java/uk/nhs/prm/e2etests/repository/CoreRepository.java
@@ -0,0 +1,44 @@
+package uk.nhs.prm.e2etests.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import uk.nhs.prm.e2etests.exception.NotFoundException;
+import uk.nhs.prm.e2etests.model.database.CoreRecord;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@Component
+public class CoreRepository {
+    private final DynamoDbTable<CoreRecord> table;
+    private static final String CORE_LAYER = "CORE";
+
+    @Autowired
+    public CoreRepository(@Value("${aws.configuration.databaseNames.transferTrackerDb}") String tableName,
+                          DynamoDbEnhancedClient dynamoDbEnhancedClient) {
+        this.table = dynamoDbEnhancedClient.table(tableName, TableSchema.fromBean(CoreRecord.class));
+    }
+
+    private Optional<CoreRecord> getRecordByInboundConversationId(String inboundConversationId) {
+        final Key key = Key.builder()
+            .partitionValue(inboundConversationId)
+            .sortValue(CORE_LAYER)
+            .build();
+
+        return Optional.ofNullable(table.getItem(key));
+    }
+
+    public void markCoreAsSoftDeleted(String inboundConversationId, Instant instant) {
+        final CoreRecord record = getRecordByInboundConversationId(inboundConversationId)
+            .orElseThrow(() -> new NotFoundException(inboundConversationId));
+
+        record.setDeletedAt((int) instant.toEpochMilli());
+
+        table.updateItem(record);
+    }
+}
